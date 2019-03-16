2b759c149b98988c705a4af2c593d604bcad3737
Jill San Luis <jill.devs@gmail.com>
2019-03-15 23:19:56 -0400

    Add call to new Ruby Provider.

--- kiddo-py/kid.py
+++ kiddo-py/kid.py
@@ -5,3 +5,7 @@ class Kid(object):
     def ask_mommy_for_food(self):
         response = requests.get('http://mommy:5000/food')
         return response.json()['answer']
+
+    def ask_daddy_for_food(self):
+        response = requests.get('http://daddy:4567/food')
+        return response.json()['answer']
--- kiddo-py/test_kid.py
+++ kiddo-py/test_kid.py
@@ -5,3 +5,9 @@ def test_kid_asks_mommy_for_food():
     kid = Kid()
     answer = kid.ask_mommy_for_food()
     assert answer == 'yes'
+
+
+def test_kid_asks_daddy_for_food():
+    kid = Kid()
+    answer = kid.ask_daddy_for_food()
+    assert answer == 'yes'
